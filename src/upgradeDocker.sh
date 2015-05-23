eeb2c155f6b8ed83f59b374122ff081981d40a39
Josh Cox <josh@webhosting.coop>
2015-05-23 18:11:28 -0500

    for loop

--- src/upgradeDocker.sh
+++ src/upgradeDocker.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-echo "overwriting /usr/local/bin/docker"
-sleep 1
-echo '!'
-sleep 1
-echo '!'
-sleep 1
-echo '!'
-sleep 1
+echo -n "overwriting /usr/local/bin/docker Ctrl-C now to exit"
+for i in {1..5};do sleep 1; echo -n '!'; done
 echo '!'
 cd /tmp
 wget https://get.docker.com/builds/Linux/x86_64/docker-latest
