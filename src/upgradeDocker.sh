a4a8be46677399fe8c3425a4664bb32feac9410b
Josh Cox <josh@webhosting.coop>
2015-05-28 11:02:50 -0500

    upgrade now has docker-compose

--- src/upgradeDocker.sh
+++ src/upgradeDocker.sh
@@ -6,3 +6,5 @@ cd /tmp
 wget https://get.docker.com/builds/Linux/x86_64/docker-latest
 chmod +x docker-latest
 sudo mv docker-latest /usr/local/bin/docker
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
