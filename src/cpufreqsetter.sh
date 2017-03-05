474b31d23c24e7cbfc2954737130f3d049a5bf5b
Josh Cox <josh@webhosting.coop>
2017-03-05 00:22:16 -0600

    swappy

--- src/cpufreqsetter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# set cpu gov
-# in a 8 core cpu environment
-for i in {0..7}; do echo "sudo cpufreq-set -c$i -g $1";done
-for i in {0..7}; do sudo cpufreq-set -c$i -g $1;done
-cpufreq-info
-
